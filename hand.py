2a908220a32f6c7952a3b83147d961e93aa2efca
LarrabeeMitchel <larrabee.mitchell@icloud.com>
2020-12-18 23:14:12 +0000

    pair and 2 pair working

--- hand.py
+++ hand.py
@@ -13,20 +13,67 @@ class Hand:
     return True
   
   def is_straight(self):
-    n = self._cards[0].n
-    for c in self._cards[1:]:
-      if c.n != n + 1:
-        return False
-      n += 1
-    return True
+    def four_in_a_row(n):
+      for c in self._cards[1:]:
+        if c.n != n + 1:
+          return False
+        n += 1
+      return True
+  
+    return (four_in_a_row(self._cards[0].n) 
+      or (self._cards[0].n == 1 and four_in_a_row(9)))
   
   def is_straight_flush(self):
-    if self.is_straight() == True and self.is_flush() == True:
-      return True
+    return self.is_straight() and self.is_flush()
+
+  def is_royal_flush(self):
+    return self.is_straight_flush() and self._cards[0].n == 1
+
+
+  def is_pair(self):
+    c = 0
+    for x in self._cards[1:]:
+      f = self._cards[c].n
+      if x.n == f:
+        return True
+      else:
+        c += 1
+    return False
+
+  def is_3_of_a_kind():
+    return False
+ 
+  def is_full_house():
+    return False
+
+  def is_4_of_a_kind():
+    return False
+
+  def is_two_pair(self):
+    r = 0
+    c = 0
+    for x in self._cards[1:]:
+      f = self._cards[c].n
+      print(f)
+      print(x.n)
+      if x.n == f:
+        r += 1
+        c += 1
+        print(r)
+        if r == 2: 
+          return True
+      else:
+        c += 1
+        print(c)
     return False
 
 
-    
+  
+   
+        
+  
+  
 
+      
   def __str__(self):
     return ", ".join([str(x) for x in self._cards])
--- main.py
+++ main.py
@@ -15,10 +15,33 @@ assert c2 < c1
 assert c1 > c2
 
 straight_flush = Hand([Card("Spades", x) for x in range(1,6)])
-assert(straight_flush.is_flush())
-assert(straight_flush.is_straight())
+assert straight_flush.is_flush()
+assert straight_flush.is_straight()
 assert straight_flush.is_straight_flush()
+
+royal_flush = Hand([Card("Spades", x) for x in range(10,14)]+[Card("Spades",1)])
+assert royal_flush.is_straight()
+assert royal_flush.is_royal_flush()
+
 not_flush = Hand([Card("Spades", x) for x in range(1, 5)]+[Card("Hearts", 1)])
 assert not(not_flush.is_flush())
 assert not(not_flush.is_straight())
 assert not not_flush.is_straight_flush()
+assert not not_flush.is_royal_flush()
+
+pair = Hand([Card(s, 8) for s in ["Hearts","Clubs"]]+[Card("Spades",x) for x in range(1,4)])
+print(pair)
+assert pair.is_pair()
+
+three = Hand([Card(s, 8) for s in ["Hearts","Clubs","Diamonds"]]+[Card("Spades",x) for x in range(1,3)])
+print(three)
+
+four = Hand([Card(s, 8) for s in ["Hearts","Clubs","Diamonds","Spades"]]+[Card("Spades",x) for x in range(1,2)])
+print(four)
+
+two_pair = Hand([Card(s, 8) for s in ["Hearts","Clubs"]]+[Card(s, 9) for s in ["Hears","Clubs"]]+[Card("Spades",1)])
+print(two_pair)
+assert two_pair.is_two_pair()
+
+full_house = Hand([Card(s, 8) for s in ["Hearts","Clubs"]]+[Card(s, 9) for s in ["Hearts","Clubs","Diamonds"]])
+print(full_house)
